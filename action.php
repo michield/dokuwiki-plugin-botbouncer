a05a9de55af17ad959019e6d30619d5bae5af95a
Michiel Dethmers <git@phplist.com>
2013-04-24 10:26:35 -0300

    add ips to call, so that the honeypotproject is actioned

--- action.php
+++ action.php
@@ -32,48 +32,56 @@ class action_plugin_botbouncer extends DokuWiki_Action_Plugin {
     }
 
     function handle_start(&$event, $param) {
-      ## handle whitelist
-      $whitelist_ips = explode(',',$this->getConf('whitelist'));
-      $whitelist_ips = array_map('trim', $whitelist_ips);
-      if (in_array($_SERVER['REMOTE_ADDR'],$whitelist_ips)) {
+        ## handle whitelist
+        $whitelist_ips = explode(',',$this->getConf('whitelist'));
+        $whitelist_ips = array_map('trim', $whitelist_ips);
+        if (in_array($_SERVER['REMOTE_ADDR'],$whitelist_ips)) {
         return;
-      }
-      
-      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $isRegister = 0;
-        if ($_REQUEST['do'] == 'register' && $_POST['save'] == 1) {
-            $email = $_POST['email'];
-            $username = $_POST['login'];
-            $content = $_POST['fullname'];
-            $isRegister = 1;
-        } else {
-            $username = $_SESSION[DOKU_COOKIE]['auth']['info']['name'];
-            $email = $_SESSION[DOKU_COOKIE]['auth']['info']['mail'];
-            $content = '';
+        }
+        $ips = array();
+        if (isset($_SERVER['REMOTE_ADDR'])) {
+          $ips[] = $_SERVER['REMOTE_ADDR'];
+          if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ips[] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+          }
         }
       
-       // error_reporting(E_ALL);
-       // ini_set('display_errors',true);
-        $honeypotApiKey = $this->getConf('honeypotapikey');
-        $akismetApiKey = $this->getConf('akismetapikey');
-        $akismetUrl = $this->getConf('akismetblogurl');
-        $mollomPublicKey = $this->getConf('mollompublickey');
-        $mollomPrivateKey = $this->getConf('mollomprivatekey');
-        $continue = $this->getConf('continue');
-        $spamError = $this->getConf('spamerror');
-        include dirname(__FILE__).'/lib/botbouncer.php';
-        $fsc = new botBouncer($honeypotApiKey,$akismetApiKey,$akismetUrl,$mollomPrivateKey,$mollomPublicKey);
-        $fsc->setLogRoot($GLOBALS['conf']['cachedir']);
-        if ($fsc->isSpam(
-          array(
-   #         'test' => 'spam',
-   #         'test' => 'ham',
-            'username' => $username,
-            'email' => $email,
-            'content' => $content,
-          ),
-          !empty($continue)
-        )) {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $isRegister = 0;
+            if ($_REQUEST['do'] == 'register' && $_POST['save'] == 1) {
+                $email = $_POST['email'];
+                $username = $_POST['login'];
+                $content = $_POST['fullname'];
+                $isRegister = 1;
+            } else {
+                $username = $_SESSION[DOKU_COOKIE]['auth']['info']['name'];
+                $email = $_SESSION[DOKU_COOKIE]['auth']['info']['mail'];
+                $content = ''; ##@@TODO find some field to pass on here
+            }
+      
+           // error_reporting(E_ALL);
+           // ini_set('display_errors',true);
+            $honeypotApiKey = $this->getConf('honeypotapikey');
+            $akismetApiKey = $this->getConf('akismetapikey');
+            $akismetUrl = $this->getConf('akismetblogurl');
+            $mollomPublicKey = $this->getConf('mollompublickey');
+            $mollomPrivateKey = $this->getConf('mollomprivatekey');
+            $continue = $this->getConf('continue');
+            $spamError = $this->getConf('spamerror');
+            include dirname(__FILE__).'/lib/botbouncer.php';
+            $fsc = new botBouncer($honeypotApiKey,$akismetApiKey,$akismetUrl,$mollomPrivateKey,$mollomPublicKey);
+            $fsc->setLogRoot($GLOBALS['conf']['cachedir']);
+            if ($fsc->isSpam(
+              array(
+        #         'test' => 'spam',
+        #         'test' => 'ham',
+                'username' => $username,
+                'email' => $email,
+                'content' => $content,
+                'ips' => $ips,
+              ),
+              !empty($continue)
+            )) {
           
             $logLine = time()."\t".$fsc->matchedBy. "\t".$fsc->matchedOn;
             unset($_POST['save']);
