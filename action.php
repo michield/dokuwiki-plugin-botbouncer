a693686eb3c856739f7d913045198473674a5994
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:00:37 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -23,7 +23,7 @@ class action_plugin_botbouncer extends DokuWiki_Action_Plugin {
     /**
      * register the eventhandlers and initialize some options
      */
-    function register(&$controller){
+    function register(Doku_Event_Handler $controller){
         $controller->register_hook('DOKUWIKI_STARTED',
                                    'BEFORE',
                                    $this,
